5db9de58c293cbedb87a6e9e73ccd8d189cc0b27
nmuelle2 <nina-marie.mueller@web.de>
2015-03-30 15:46:55 +0200

    new names

--- happiness-index-application/src/main/java/Application.java
+++ happiness-index-application/src/main/java/Application.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "de.happyindex")
+@ComponentScan(basePackages = "de.HappinessIndex")
 @EnableAutoConfiguration
 public class Application {
 
