529a991e56166ecf9e29aafa7445a706c6fad0ec
nmuelle2 <nina-marie.mueller@axelspringer.de>
2015-04-16 08:13:00 +0200

    no nexus

--- happiness-index-application/src/main/java/Application.java
+++ happiness-index-application/src/main/java/Application.java
@@ -2,8 +2,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 @ComponentScan(basePackages = "de.HappinessIndex")
 @EnableAutoConfiguration
 public class Application {
